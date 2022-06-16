333df93de850290b9b12babe1678377ea50c108e
dusanr14 <dusanr14@gmail.com>
2022-06-16 20:02:35 +0200

    dodat zadnji

--- temperatura/ds18b20_.c
+++ temperatura/ds18b20_.c
@@ -17,20 +17,19 @@ int main(int argc, char *argv[])
 	scanf("%d", &temperaturica);
 	printf("\n");
 	temp = temperaturica * 1000;
-	
+//////////////////////////////////////////////	
 	wiringPiSetup();
 	pinMode (LED,OUTPUT);
 int fd = -1, ret;
 char *tmp1, tmp2[10], ch='t';
-char devname_head[50] =
-"/sys/devices/w1_bus_master1/28-00000";
+char devname_head[50] ="/sys/devices/w1_bus_master1/28-00000";
 char devname_end[10] = "/w1_slave";
 char dev_name[100];
 long value;
 int integer, decimal;
 char buffer[100];
 int i,j;
-////////////////////
+
 strcpy(dev_name, devname_head);
 strcat(dev_name, argv[1]);
 strcat(dev_name, devname_end);
@@ -51,7 +50,7 @@ value = atoi(tmp2);
 integer = value / 1000;
 decimal = value % 1000;
 printf("Temperatura je %d.%d\n", integer,decimal);
-
+/////////////////////////////////////////////////////////////////////////
 if(value > temp)
 {
 	digitalWrite (LED, 1);
